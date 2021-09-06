46aed8af4dc294a2ed2c54a54d176c9081eb6bdf
LucianoBentrup <70353582+LucianoBentrup@users.noreply.github.com>
2021-09-06 10:49:50 -0300

    Create Calculadora IMC.cpp

---
+++ Calculadora IMC.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namestapce std;
+  
+void main {
+  
+    //Caculadora IMC
+    //Peso / altura * altura
+    //acima da media > 18.5
+    //Na media 18.5-24.9
+    //Abaixo da media < 25
+  
+system("pause>0");
