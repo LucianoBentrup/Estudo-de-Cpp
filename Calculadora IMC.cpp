3827b79873da8701faa481328850be70cddd2dc5
LucianoBentrup <70353582+LucianoBentrup@users.noreply.github.com>
2021-09-06 12:05:07 -0300

    Pronto

--- Calculadora IMC.cpp
+++ Calculadora IMC.cpp
@@ -9,4 +9,36 @@ void main {
     //Na media 18.5-24.9
     //Abaixo da media < 25
   
-system("pause>0");
+  
+    float altura,peso,imc;
+    
+    
+    cout << "Use pontos no lugar de virgulas" << endl;
+    
+  
+    cout << "Insira sua altura: ";
+    cin >> altura;
+    
+    
+    cout << "Insira o seu peso: ";
+    cin >> peso;
+
+  
+    imc = peso / (altura * altura);
+
+    
+  
+    if (imc < 18.5)
+        cout << "Voce esta abaixo da media" << endl;
+    else if (imc > 25)
+        cout << "Voce esta acima da media" << endl;
+    else 
+        cout << "Voce esta na media!" << endl;
+
+
+    cout << "IMC = " << imc << endl;
+     
+
+    system("pause>0");
+}
+  
