7367e0516ee2bb3ff39ad4eca392fb5497ac391a
LucianoBentrup <lucianobentrup@gmail.com>
2021-09-08 14:28:47 -0300

    Update Switch.cpp

--- Switch.cpp
+++ Switch.cpp
@@ -7,6 +7,14 @@ int main()
 //Programa para verificar quantos dias um mes tem
 // (ano % 4 == 0 && ano % 100 != 0 || ano % 400 == 0) 
 
+int ano, mes;
+
+cout << "***Verifique quantos dias tem o mês***";
+cout << "Insira o ano: " << endl;
+cin >> ano;
+cout << "Insira o mês:" << endl;
+cin >> mes;
+
 
 
 }
