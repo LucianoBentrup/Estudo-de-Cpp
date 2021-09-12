ee85a60a7f07736cf31b2c43ba426fb4d53f9972
LucianoBentrup <lucianobentrup@gmail.com>
2021-09-12 12:37:22 -0300

    Update whileLoopExerc.cpp

--- whileLoopExerc.cpp
+++ whileLoopExerc.cpp
@@ -1,5 +1,22 @@
 #include <iostream>
 using namespace std; 
 
-    // Escreva todos numeros entre 100-500 que podem ser divididos por 3 
+    // Escreva todos numeros entre 100-500 que podem ser divididos por 3 e 5 
 
+int main(){
+
+int counter;
+counter=100;
+
+    while (counter<=500)
+    {
+        if (counter % 3==0 && counter % 5==0)
+        {
+            cout << "O numero " << counter << "é divisivel por 3 e 5";
+        }// Fim if
+    counter++; 
+    }// Fim while
+    
+
+
+}// Fim int main
