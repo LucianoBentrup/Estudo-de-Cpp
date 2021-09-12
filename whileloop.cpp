50b9b53af6be60b86708c68ad112ebda09726b48
LucianoBentrup <lucianobentrup@gmail.com>
2021-09-12 11:49:36 -0300

    Update whileloop.cpp

--- whileloop.cpp
+++ whileloop.cpp
@@ -1,1 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    int num;
+
+cout << "Contador de digitos" << endl;
+cout << "Digite um numero:"; 
+cin >> num; 
+
+    if(num==0)
+        cout << "Voce inseriu 0";
+    else 
+    {
+        
+        int counter=0;
+        while (num>0){
+            num = num/10; 
+            counter++;
+
+        }//Fim while.
+
+    cout << "O numero que voce digitou possui" << counter << "digitos";
+
+    }//Fim else
+
+
+
+
+
+}
+
 
