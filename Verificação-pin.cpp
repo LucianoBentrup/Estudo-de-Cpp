bc68d39052ea766d77e9f1924ba2189bf694addf
LucianoBentrup <lucianobentrup@gmail.com>
2021-09-18 15:32:21 -0300

    Update Verificação-pin.cpp

--- Verificação-pin.cpp
+++ Verificação-pin.cpp
@@ -4,6 +4,15 @@ using namespace std;
 void main()
 {
 
-    int userpin=1234, pin=0;
+    int userpin=1234, pin=0, contadorerro=0;
+
+    do {
+        cout << "Insira a senha correta:";
+        cin >> pin; 
+        if(pin!=userpin)
+        contadorerro++;
+
+
+    }while(contadorerro<=3 && pin!=userpin); 
 
 }
