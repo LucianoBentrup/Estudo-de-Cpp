148c92be5550ba5e182ba57822cfa64e6d051385
LucianoBentrup <lucianobentrup@gmail.com>
2021-09-19 09:07:04 -0300

    Update mfatorial.cpp

--- mfatorial.cpp
+++ mfatorial.cpp
@@ -3,8 +3,28 @@ using namespace std;
 
 int main()
 {
-    int num;
+    int num,mult=1,soma=1;
+    
     cout << "Insira um numero:" << endl;
     cin >> num;
-    
+
+    while(num!=0)
+    {
+        if(num>=1)
+        {
+            num = num - 1;
+
+        }
+
+        else
+        {
+            num = num + 1;
+        }
+
+
+
+    }
+
+
+
 }
