5b9b6e921912b719ac8029e4281bfa756a78c7dc
LucianoBentrup <lucianobentrup@gmail.com>
2021-09-19 10:30:53 -0300

    Update CalculadoraFunc.cpp

--- CalculadoraFunc.cpp
+++ CalculadoraFunc.cpp
@@ -46,5 +46,77 @@ void divisao()
 
 void modulado()
 {
+    int num,modulo,resultado;
+    cout << "Insira o numero: ";
+    cin >> num;
+    cout << "Insira o modulo: ";
+    cin >> modulo;
+
+    bool NumInt, moduloInt; 
+            NumInt=((int)num == num);
+            moduloInt=((int)modulo == modulo);
+
+            if (NumInt && moduloInt)
+            {
+               resultado = (int)num % (int)modulo;
+               cout << "O resultado é: " << resultado;
+            }
+            else
+            {
+                cout << "Invalido!";
+            }
+                
+}
+
+void fatorial()
+{
+    int num,resultado=1; 
+
+    cout << "Insira um numero: ";
+    cin >> num;
+
+    for(int x=1;x<=num;x++)
+    {
+        resultado = resultado * x;
+    }
+
+cout << "O resultado de " << num << "! é " << resultado; 
+
+}
+
+void Calculadora()
+{
+    int opcao=0;
+    while(opcao!=9)
+    {
+        cout << "===============Menu===============" << endl;
+        cout << "Opcoes:" << endl;
+        cout << "1 - Soma" <<endl;
+        cout << "2 - Subtracao" << endl; 
+        cout << "3 - Multiplicacao" << endl; 
+        cout << "4 - divisao" << endl; 
+        cout << "5 - Modulo" << endl; 
+        cout << "6 - Fatorial" << endl;
+        cout << "9 - Sair" << endl;
+        cout << "Qual opcao voce deseja executar:";
+        cin >> opcao;
+
+        switch(opcao)
+        {
+            case 1: soma(); break;
+            case 2: subtracao(); break;
+            case 3: multiplicacao(); break;
+            case 4: divisao(); break;
+            case 5: modulado(); break; 
+            case 6: fatorial(); break;
+            case 9: "Obrigado por usar minha calculadora"; break;
+            default: "Insira uma opcao valida!"; break;
+        }
+    }
+}
+
+
+int main()
+{
 
 }
